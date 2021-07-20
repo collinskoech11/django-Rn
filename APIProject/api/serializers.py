be5bdbeac4c7dddf1634508e146ab8d5a589e2cd
collinskoech11 <collinskoechck34@gmail.com>
2021-07-20 20:05:09 +0300

    added serializers

---
+++ APIProject/api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Article
+
+class ArticleSerializers(serializers.Serializers):
+    title = serializers.CharField(max_length=100)
+    description = serializers.TextField()
