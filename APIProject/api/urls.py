664d27eeed19e30a17806fa28a42916602812366
collinskoech11 <collinskoechck34@gmail.com>
2021-07-21 09:29:52 +0300

    display article list

--- APIProject/api/urls.py
+++ APIProject/api/urls.py
@@ -1,8 +1,8 @@
 
 
 from django.urls import path
-from .views import Index
+from .views import article_list
 
 urlpatterns = [
-    path('', Index),
+    path('articles/', article_list),
 ]
--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render,HttpResponse
+from .models import Article
+from .serializers import ArticleSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
 
-def Index(request):
-    return HttpResponse("Its working")
+def article_list(request):
+
+
+    #get all articles 
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe = False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
