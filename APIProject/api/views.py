4e772f2b66c66e1bdaa613944b47993bad6855e9
collinskoech11 <collinskoechck34@gmail.com>
2021-07-22 09:05:46 +0300

    update dara serializers

--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -3,10 +3,11 @@ from .models import Article
 from .serializers import ArticleSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-
+@csrf_exempt
 def article_list(request):
 
 
@@ -17,6 +18,26 @@ def article_list(request):
         return JsonResponse(serializer.data, safe = False)
 
     elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def article_details(request, pk):
+    try: 
+        article =  Article.objects.get(pk=pk)
+
+    except:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = ArticleSerializer(data=data)
         if serializer.is_valid():
