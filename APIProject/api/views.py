ce8f527a36397d3fff14f163a52d4a4fd0c20481
collinskoech11 <collinskoechck34@gmail.com>
2021-07-22 15:02:28 +0300

    delete function

--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -35,9 +35,23 @@ class ArticleDetails(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def get(self, request, id):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
+        articles = self.get_object(id)
+        serializer = ArticleSerializer(articles)
         return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 
 '''
